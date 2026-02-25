6727cce302cc79adb61221cf8dd16223e7595804
jmb <juanmiguelluna2004@gmail.com>
2026-02-26 02:02:04 +0800

    big push (CHECK ON LAYER AND MASKS)
    - changed collisions from code to layers and masks
    
    layer 1: Player
    layer 2: Fighter
    layer 3: PlayerProjectile
    layer 4: FighterProjectile
    
    done with: player death, explosions on hit
    
    need: enemies shooting, enemies spawning

--- 2d-shoot-em-up/scripts/fighter.gd
+++ 2d-shoot-em-up/scripts/fighter.gd
@@ -1,24 +1,20 @@
 extends Area2D
 
 var health: int = 3
-var speed: float =  100.0
+var speed: float = 100.0
 var velocity = Vector2(0,1)
 var random_timer: float = 0.0
+
 @onready var anim = $AnimatedSprite2D
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	velocity.x = randf_range(-1.0, 1.0)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	random_timer -= delta
 	if random_timer <= 0.0:
-		# Pick a new random horizontal speed and direction
 		velocity.x = randf_range(-1.0, 1.0)
-		# Set the timer to wait a random amount of time (between 0.5 and 1.5 seconds) before changing again
 		random_timer = randf_range(0.5, 1.5)
-		
 	position += velocity * speed * delta
 	
 	var screen_size = get_viewport_rect().size
@@ -26,26 +22,34 @@ func _process(delta: float) -> void:
 	if position.x < 20 or position.x > screen_size.x - 20:
 		velocity.x *= -1 
 		position.x = clamp(position.x, 20, screen_size.x - 20) 
-		
+
 	if position.y > screen_size.y + 50:
 		queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("projectile"):
-		area.queue_free()
 		if health > 0:
 			health -= 1
+
 			if health <= 0:
 				speed = 0
+				set_deferred("monitoring", false)
+				set_deferred("monitorable", false)
 				anim.play("Death")
 				await anim.animation_finished
 				queue_free()
-		
+
+			else:
+				anim.play("Hit") 
+				await get_tree().create_timer(0.1).timeout
+				if health > 0:
+					anim.play("Normal") 
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		if health > 0:
-			health = 0
-			speed = 0
-			anim.play("Death")
-			await anim.animation_finished
-			queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage(1)
+
+	health = 0
+	speed = 0
+	anim.play("Death")
+	await anim.animation_finished
+	queue_free()
--- 2d-shoot-em-up/scripts/player.gd
+++ 2d-shoot-em-up/scripts/player.gd
@@ -2,23 +2,26 @@ extends CharacterBody2D
 
 @export var speed = 300
 @export var fire_rate = 0.2
+@export var health: int = 5 
 var fire_timer: float = 0.0
+
+@onready var anim = $AnimatedSprite2D 
 const PROJECTILE = preload("uid://d337ap0wrf3wp")
 
 func _process(delta: float) -> void:
-	var move = Input.get_vector("left","right","up","down")
+	var move = Input.get_vector("left", "right", "up", "down")
 	if move:
 		velocity = move * speed
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
-	
-	# Preventing going off screen
+
 	var screen_size = get_viewport_rect().size
 	var padding = 20
 	global_position.x = clamp(global_position.x, padding, screen_size.x - padding)
 	global_position.y = clamp(global_position.y, padding, screen_size.y - padding)
 	
+	# Firing logic
 	if fire_timer > 0.0:
 		fire_timer -= delta
 	if Input.is_action_pressed("shoot") and fire_timer <= 0.0:
@@ -29,3 +32,38 @@ func shoot() -> void:
 	var new_projectile = PROJECTILE.instantiate()
 	new_projectile.global_position = global_position
 	add_sibling(new_projectile)
+
+func _on_area_entered(area: Area2D) -> void:
+	area.queue_free() 
+
+	if health > 0:
+		health -= 1
+		if health <= 0:
+			speed = 0
+			set_deferred("monitoring", false)
+			set_deferred("monitorable", false)
+			anim.play("Death")
+			await anim.animation_finished
+			queue_free()
+		else:
+			anim.play("Hit") 
+			await get_tree().create_timer(0.1).timeout
+			if health > 0:
+				anim.play("Normal")
+
+func take_damage(amount: int):
+	health -= amount
+	print("Player Health: ", health)
+	if health <= 0:
+		die()
+	else:
+		anim.play("Hit")
+		await get_tree().create_timer(0.1).timeout
+
+		anim.play("Normal")
+
+func die():
+	set_process(false) 
+	anim.play("Death")
+	await anim.animation_finished
+	get_tree().reload_current_scene()
